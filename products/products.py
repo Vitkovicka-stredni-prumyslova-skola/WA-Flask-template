de140a3ad734c3d586142b4db9e5fa0c163d1ea4
Martin-Kapcala <martin.kapcala@vitkovickastredni.cz>
2024-03-04 09:01:58 +0100

    insert form html for add new products

--- products/products.py
+++ products/products.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from API.api import GetAllProducts, GetSingleProducts
+
 products_bp = Blueprint('products_bp', __name__,
     template_folder='templates',
     static_folder='static')
@@ -16,3 +17,10 @@ def detailOfProduct(id):
     data = GetSingleProducts(id)
 
     return render_template('products/detail.html', detailOfPorduct = data)
+
+#def getRelatedProducts(id):
+  #  allProducts = GetAllProducts()
+
+@products_bp.route('/products/add')
+def addProduct():
+    return render_template('products/new-product.html')    
